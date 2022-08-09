325042f592d01f0556906b318fb159c856b1240e
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-08-09 12:32:37 +0600

    views_computer

--- computer/views.py
+++ computer/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Computer
+from .serializers import ComputerSerializer
 
-# Create your views here.
+class ComputerViewSet(ModelViewSet):
+    queryset = Computer.objects.all()
+    serializer_class = ComputerSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
+
