5c64e0f3e6778b9a679280922612bb83cc8da1c3
hello <dedmorozimran1997@gmail.com>
2022-08-09 12:26:10 +0600

    models

--- computer/models.py
+++ computer/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+# модельки
+# Компьютер (модель, кол-во ядер(2, 4, 8, 16), оператвная память (2-120), цена, фото)
+# Рейтинг (пользователь, компьютер, значение(1-5))
+
+class Computer(models.Model):
+    model = models.CharField(max_length=50)
+    count_core = models.IntegerField(choices=[(2,2), (4,4), (8,8), (16,16)])
+    ram = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+     )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='computers', blank=True, null=True)
+
+
+
+
+    @property
+    def average_rating(self):
+        ratings = [rating.value for rating in self.ratings.all()]
+        if ratings:
+            return sum(ratings)/len(ratings)
+        else:
+            return 0
