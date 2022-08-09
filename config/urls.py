7e27cbff547f9fd5998925b7fd21fb8c65787699
Arafat <arafatamrullaev021@gmail.com>
2022-08-09 15:35:35 +0600

    mediastatic

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+from config.settings import STATIC_ROOT
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('computer.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
